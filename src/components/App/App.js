eae7c934863a2f7b9ca052035952ee19eae9b1c2
Piotr Witkowski <pwitk@wp.pl>
2022-01-26 17:45:22 +0100

    Add ESLint and husky

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,8 +10,8 @@ class App extends React.Component {
   state = {
     lists: listData || [],
     hamburgerContent: 'zakryj',
-    listVisibility: ''
-  }
+    listVisibility: '',
+  };
   
   addList(title) {
     this.setState(state => (
@@ -24,21 +24,21 @@ class App extends React.Component {
             description: 'All my dreams',
             image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
             imageDescription: 'Rocket in space',
-            columns: []
-          }
-        ]
+            columns: [],
+          },
+        ],
       }
     ));
   }
 
   handleHamburgerClick = () => {
-      const isVisible = this.state.listVisibility == '' ? 'invisibility' : '';
-      console.log(isVisible);
+    const isVisible = this.state.listVisibility === '' ? 'invisibility' : '';
+    console.log(isVisible);
     this.setState({
-      hamburgerContent: 'odkryj',
+      hamburgerContent: this.state.hamburgerContent === 'zakryj' ? 'odkryj' : 'zakryj',
       listVisibility: isVisible,
     });
-  }
+  };
 
   render() {
     console.log(this.state.hamburgerContent);
@@ -47,26 +47,26 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{pageContents.title}</h1> 
         <h2 className={styles.subtitle}>{pageContents.subtitle}</h2> 
+        <div>
+          <Hamburger content={this.state.hamburgerContent} onClick={this.handleHamburgerClick}>
+            {this.state.hamburgerContent}
+          </Hamburger>
+        </div>
+        <section className={styles[this.state.listVisibility]}>
           <div>
-            <Hamburger onClick={this.handleHamburgerClick}>
-              {this.state.hamburgerContent}
-            </Hamburger>
+            {this.state.lists.map(({key, ...listProps})=>(
+              <List key={key} {...listProps} />
+            ))}
           </div>
-          <section className={styles[this.state.listVisibility]}>
-            <div>
-              {this.state.lists.map(({key, ...listProps})=>(
-                  <List key={key} {...listProps} />
-              ))}
-            </div>
 
-            {/*<List {...lists}/>*/}
-            <Creator 
-              text={settings.listCreatorText}
-              action={title => this.addList(title)} 
-            />
-          </section>
+          {/*<List {...lists}/>*/}
+          <Creator 
+            text={settings.listCreatorText}
+            action={title => this.addList(title)} 
+          />
+        </section>
       </main>
-    )
+    );
   }
 }
 
