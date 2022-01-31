830ec65ead68c01ad7755c943fb517bde76196b8
Piotr Witkowski <pwitk@wp.pl>
2022-01-31 15:26:53 +0100

    Add cardsRedux.js and columnsRedux.js. Modified store.js, List.js, ListContainer.js, Colmn.js and ColumnContainet.j - to add functionality of creation card and columns. Install shortid.

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-//import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Card from '../Card/Card.js';
-//import {settings} from '../../data/dataStore.js';
+import {settings} from '../../data/dataStore.js';
 import Icon from '../Icon/Icon.js';
 
 
@@ -12,10 +12,15 @@ class Column extends React.Component {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string,
     cards: PropTypes.array,
+    addCard: PropTypes.func,
+  };
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
   };
 
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
@@ -28,12 +33,11 @@ class Column extends React.Component {
         {cards.map(cardData => (
           <Card key={cardData.id} {...cardData} />
         ))}
-        {/*
+        
         <Creator 
           text={settings.cardCreatorText}
-          action={title => this.addCard(title)} 
+          action={addCard} 
         />
-        */}
       </section>
     );
   }
--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -1,10 +1,19 @@
 import {connect} from 'react-redux';
 import List from './List';
+import { getColumnsForList, createActionAddColumn} from '../../redux/columnsRedux';
 
-export const getColumnsForList = ({columns}, listId) => columns.filter(column => column.listId == listId);
-
+//change state (from store) to props for List component
 const mapStateToProps = (state, props) => ({
   columns: getColumnsForList(state, props.id),
 });
 
-export default connect(mapStateToProps)(List);
+//this function returns methods which we can use in our List component to dispach actions
+const mapDispatchToProps = (dispatch, props) => ({
+  addColumn: title => dispatch(createActionAddColumn({
+    listId: props.id,
+    title,
+  })),
+});
+
+//connect function links two world: react and redux. In second () we write component name for which we prepared components
+export default connect(mapStateToProps, mapDispatchToProps)(List);
